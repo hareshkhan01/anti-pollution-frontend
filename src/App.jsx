506a4217614a5ac46f2f69019f5fe2859516658e
hareshkhan01 <mrhitman060@gmail.com>
2026-03-21 15:15:08 +0530

    Solve Dependency Array

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ function App() {
   useEffect(() => {
     const olaMaps = new OlaMaps({
       apiKey: '90NQ4qVGTY4rF9fkXnyyqz432I3drMT9Ujqe09mN',
-    });
+    }, []);
 
     olaMaps.init({
       style: "https://api.olamaps.io/tiles/vector/v1/styles/default-light-standard/style.json",
       container: 'map',
-      center: [77.61648476788898, 12.931423492103944],
-      zoom: 15,
+      center: [22.5742293, 88.4340046],
+      zoom: 5,
     });
   }, []);
 
