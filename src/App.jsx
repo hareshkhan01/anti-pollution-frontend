2dd73b658bd9fa455c0c49c1872b3ce1b908640c
hareshkhan01 <mrhitman060@gmail.com>
2026-03-21 14:32:24 +0530

    Setup Map SDK

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect } from 'react';
+import './App.css';
+import { OlaMaps } from 'olamaps-web-sdk';
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const olaMaps = new OlaMaps({
+      apiKey: '90NQ4qVGTY4rF9fkXnyyqz432I3drMT9Ujqe09mN',
+    });
+
+    olaMaps.init({
+      style: "https://api.olamaps.io/tiles/vector/v1/styles/default-light-standard/style.json",
+      container: 'map',
+      center: [77.61648476788898, 12.931423492103944],
+      zoom: 15,
+    });
+  }, []);
 
   return (
-   <>
-   <h1>Hello Map</h1>
-   </>
-  )
+    <>
+      <h1>Hello Map</h1>
+      <div id="map" style={{ width: '100vw', height: '100vh' }}></div>
+    </>
+  );
 }
 
-export default App
+export default App;
